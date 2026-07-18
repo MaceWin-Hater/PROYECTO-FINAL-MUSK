afa66764503dba33f33a0a621b79fc17b3d3d945
MaceWin-Hater <correosecundariorandomxdlol@gmail.com>
2026-07-18 16:56:45 +0200

    Update analyze.py

--- src/analyze.py
+++ src/analyze.py
@@ -4,3 +4,10 @@ import csv
 def generate_report():
   with open("data/clients.json", "r") as file:
     clients = json.load(file)
+
+  sales = []
+  with open("data/sales.csv", "r") as file:
+    reader = csv.DictReader(file)
+    
+    for row in reader:
+      sales.append(row)
