7b72e3017bc0d8ef2d6c2d33de3649dafa6b9320
MaceWin-Hater <correosecundariorandomxdlol@gmail.com>
2026-07-18 16:32:32 +0200

    Create sale.py

---
+++ src/sale.py
@@ -0,0 +1,9 @@
+class Sale:
+
+    def __init__(self, sale_id, client_id, product, category, amount, date):
+        self.sale_id = sale_id
+        self.client_id = client_id
+        self.product = product
+        self.category = category
+        self.amount = amount
+        self.date = date
