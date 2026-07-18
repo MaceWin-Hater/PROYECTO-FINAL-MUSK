b1a52702aff93ae71575940b4eee4cec3415c6fd
MaceWin-Hater <correosecundariorandomxdlol@gmail.com>
2026-07-18 16:30:39 +0200

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,1 +1,6 @@
-
+class Client:
+    def __init__(self, client_id, name, country, signup_date):
+        self.client_id = int(client_id)
+        self.name = name
+        self country = country
+        self signup_date = signup_date
