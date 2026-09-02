e17018776993852b65a4c2f694ac74454fc65479
MaceWin-Hater <correosecundariorandomxdlol@gmail.com>
2026-09-02 20:26:06 +0200

    Create sales_collection.py

---
+++ src/sales_collection.py
@@ -0,0 +1,18 @@
+class SalesCollection:
+    def __init__(self, sales):
+        self.sales = sales
+
+    def sales_by_client(self, client_id):
+        return [s for s in self.sales if s.client_id == client_id]
+
+    def total_amount_by_client(self, client_id):
+        return sum(s.amount for s in self.sales_by_client(client_id))
+
+    def total_amount_by_category(self, category):
+        return sum(s.amount for s in self.sales if s.category == category)
+
+    def average_sale_by_client(self, client_id):
+        client_sales = self.sales_by_client(client_id)
+        if not client_sales:
+            return 0
+        return self.total_amount_by_client(client_id) / len(client_sales)
